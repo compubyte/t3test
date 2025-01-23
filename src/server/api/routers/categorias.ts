37428a3b119a3eddbe801bf0d7e674d3f5478786
compubyte <curicomputacion@hotmail.com>
2025-01-22 21:40:16 -0300

    Cuarto commit

---
+++ src/server/api/routers/categorias.ts
@@ -0,0 +1,42 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "@/server/api/trpc";
+import { posts } from "@/server/db/schema";
+
+export const categoriasRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+
+  create: protectedProcedure
+    .input(z.object({ name: z.string().min(1) }))
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.insert(posts).values({
+        name: input.name,
+        createdById: ctx.session.user.id,
+      });
+    }),
+
+  getLatest: protectedProcedure.query(async ({ ctx }) => {
+    const post = await ctx.db.query.posts.findFirst({
+      orderBy: (posts, { desc }) => [desc(posts.createdAt)],
+    });
+
+    return post ?? null;
+  }),
+
+  getList: protectedProcedure.query(async ({ ctx }) => {
+    const listaCategorias = await ctx.db.query.tablaCategorias.findMany({
+      orderBy: (tablaCategorias, { desc }) => [desc(tablaCategorias.id)],
+    });
+    return listaCategorias ?? [];
+  }),
+});
